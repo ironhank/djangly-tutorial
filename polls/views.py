8f53bc1b76bf880711bf409a8b9b9e7e3722931b
ironhank <rhmushinski@gmail.com>
2014-11-05 12:48:08 -0500

    fixed 'vote again' url

--- polls/views.py
+++ polls/views.py
@@ -7,12 +7,6 @@ from django.core.urlresolvers import reverse
 
 from polls.models import Poll, Choice
 
-# def index(request):
-# 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-# 	template = loader.get_template('polls/index.html')
-# 	context = RequestContext(request,
-# 		{'latest_poll_list':latest_poll_list})
-# 	return HttpResponse(template.render(context))
 
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -24,7 +18,8 @@ def detail(request, poll_id):
 	return render(request, 'polls/detail.html', {'poll':poll})
 
 def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s" % poll_id)
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render(request, 'polls/results.html', {'poll':poll})
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
@@ -39,4 +34,4 @@ def vote(request, poll_id):
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
-		return HttpResponse(reverse('polls:results', args=(p.id,)))
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
